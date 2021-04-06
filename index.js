4bb3dfad3b2addb1fad46305b05fe6e83d153516
yseto <yseto@users.noreply.github.com>
2021-04-06 19:22:55 +0900

    w

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require("@actions/github");
+const fs = require("fs");
 
 async function run() {
   try {
     const token = core.getInput('token');
-    core.info("start");
+    const mode = core.getInput('mode');
 
+    if (mode === 'start') {
+      core.info("start");
+      start();
+    } else if (mode === 'finish') {
+      core.info("finish");
+      finish(token);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function start() {
     const seconds = Math.floor(Date.now() / 1000);
     const branchName = `branch-${seconds}`;
     await exec.exec('git', ['checkout', '-b', branchName]);
 
-    core.saveState("seconds", seconds);
-    core.saveState("branchName", branchName);
-    core.saveState("token", token);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    core.setOutput("seconds", seconds);
+    core.setOutput("branchName", branchName);
+}
+
+async function finish(token) {
+    const octokit = github.getOctokit(token);
+    const { repo: { owner, repo } } = github.context;
+
+    const seconds = core.getInput("seconds");
+
+    const branchName = core.getInput("branchName");
+
+    await fs.promises.writeFile("seconds.txt", `seconds: ${seconds}`, {encoding: "utf8"});
+
+    await exec.exec("git", ["add", "."]);
+    const message = `version: ${seconds}`;
+    await exec.exec('git', ['commit', '-m', message]);
+    await exec.exec('git', ['push', 'origin', branchName]);
+
+    await octokit.pulls.create({
+      owner,
+      repo,
+      head: `${owner}:${branchName}`,
+      base: 'main',
+      body: message,
+      title: "testing create PR",
+    });
+ 
 }
 
 run();
